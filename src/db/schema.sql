bb96d57c2df0f225e17ef88cfa382353bc20b39a
Edward Lim <ed.lim199@gmail.com>
2026-06-14 16:23:36 +1000

    confident with the postgres script

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,93 +1,89 @@
-PRAGMA journal_mode = WAL;
-PRAGMA foreign_keys = ON;
+BEGIN;
 
-CREATE TABLE IF NOT EXISTS unit_of_measurement (
-  unit TEXT PRIMARY KEY NOT NULL 
+-- enums
+CREATE TYPE IF NOT EXISTS unit_of_measurement AS ENUM (
+  "Each", "Kg", "g", "L", "mL", "SS" 
 );
-
-INSERT INTO unit_of_measurement ("Each", "Kg", "g", "L", "mL");
 
 -- domain data
 CREATE TABLE IF NOT EXISTS retailers (
-  name TEXT PRIMARY KEY NOT NULL,
-  total_products INTEGER DEFAULT 0
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+  name TEXT NOT NULL UNIQUE,
+  total_products INTEGER DEFAULT 0,
+  url TEXT NOT NULL UNIQUE
 );
-INSERT INTO retailers(name) VALUES ('woolworths', 'coles');
+INSERT INTO retailers (name, url) VALUES ('woolworths', 'https//:woolworths.com.au') ('coles', 'https://coles.com.au');
 
 CREATE TABLE IF NOT EXISTS categories (
-  retailer TEXT NOT NULL,
+  -- natural keys and surrogate id
+  retailer_id INTEGER NOT NULL,
+  path TEXT NOT NULL,
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
   retailer_designated_id TEXT NOT NULL,
   name TEXT NOT NULL,
-  path TEXT NOT NULL,
   retailer_designated_product_count INTEGER DEFAULT NULL, -- this pertains to coles, who has this data in their json
  
-  FOREIGN KEY retailer REFERENCES retailers(name)
+  FOREIGN KEY retailer_id REFERENCES retailers(id),
+  UNIQUE (retailer_id, path)
 );
 
 CREATE TABLE IF NOT EXISTS products (
-  retailer INTEGER NOT NULL,
+  -- natural keys and surrogate id
+  category_id INTEGER NOT NULL,
   retailer_product_id TEXT NOT NULL,
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
   cross_retailer_id TEXT,
-  gtin_format INTEGER
-
+  gtin_format INTEGER,
   current_value INTEGER NOT NULL,
-
   name TEXT NOT NULL,
   brand TEXT,
   path TEXT NOT NULL,
   description TEXT NOT NULL,
   image_url TEXT,
 
-  FOREIGN KEY retailer REFERENCES retailers (name),
-  FOREIGN KEY value REFERENCES value_at_time (id),
-  PRIMARY KEY (retailer, retailer_product_id),
-  CHECK ((cross_retailer_id IS NOT NULL AND gtin_format IS NOT NULL) OR cross_retailer_id IS NULL)
+  FOREIGN KEY category_id REFERENCES categories (id),
+  FOREIGN KEY current_value REFERENCES value_at_time (id),
+  UNIQUE (category_id, retailer_product_id),
+  CHECK ((cross_retailer_id IS NOT NULL AND gtin_format IS NOT NULL) OR (cross_retailer_id IS NULL AND gtin_format is NULL))
 );
 
 CREATE TABLE IF NOT EXISTS value_at_time (
-  id PRIMARY KEY AUTOINCREMENT,
-
-  retailer INTEGER NOT NULL,
-  retailer_product_id INTEGER NOT NULL,
+  -- natural key (time, product_id), surrogate id and foreign key (category_scrape_id)
+  product_id INTEGER NOT NULL,
+  time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  category_scrape_id INTEGER NOT NULL,
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, -- think about scale, 365 days x 5 years x how many products
 
   -- unit price for comparison regardless of whether the product is discrete or non-discrete
-  unit_price REAL NOT NULL,
+  unit_price NUMERIC(6, 2) NOT NULL,
   -- <unit_price_quantity> of <unit_price_measure_quantity> <unit_price_unit>
-  unit_price_quantity REAL NOT NULL,
-  unit_price_measure_quantity REAL NOT NULL,
-  unit_price_unit TEXT NOT NULL,
+  unit_price_quantity INTEGER NOT NULL, -- woolworths: 1 (by default), coles: pricing.unit.quantity
+  unit_price_measure_quantity INTEGER NOT NULL, -- woolworths: CupMeasure (first part before alphanumeric), coles: pricing.unit.ofMeasureQuantity
+  unit_price_unit unit_of_measurement NOT NULL, -- woolworths: CupMeasure (alphanumeric part), coles: pricing.unit.ofMeasureUnits
 
   -- size and price of the product
-  size_unit TEXT NOT NULL,
-  size_quantity REAL,
-  size_quantity_min REAL,
-  size_quantity_max REAL,
-  price REAL NOT NULL,
+  size_unit unit_of_measurement NOT NULL, -- woolworths: PackageSize (unit part)
+  size_quantity NUMERIC(6, 2) NOT NULL, -- woolworths: PackageSize (will not be a perfect number can be, "125g x 2 pack" "per 150g", "180g")
+  size_quantity_min NUMERIC(6, 2) NOT NULL, -- woolworths: MinimumQuantity, coles: (doesn't have)
+  price NUMERIC(6, 2) NOT NULL, -- woolworths: price, coles: pricing.now
 
-  time TEXT NOT NULL,
-
-  run INTEGER NOT NULL,
-
-  FOREIGN KEY (retailer, retailer_product_id) REFERENCES products (retailer, retailer_product_id),
-  FOREIGN KEY (unit_price_unit) REFERENCES unit_of_measurement (unit),
-  FOREIGN KEY (size_unit) REFERENCES unit_of_measurement (unit),
-  FOREIGN KEY (run) REFERENCES scrape_runs (id),
-
-  CHECK (
-	(size_quantity is NOT NULL AND size_quantity_min IS NULL AND size_quantity_max is NULL)
-	OR
-	(size_quantity is NULL AND size_quantity_min IS NOT NULL AND size_quantity_max is NOT NULL)
-  )
+  FOREIGN KEY (product_id) REFERENCES products (id),
+  FOREIGN KEY (category_scrape_id) REFERENCES category_scrapes (id),
+  UNIQUE (product_id, time)
+) WITH (
+  tsdb.hypertable,
 );
 
 -- analytic/operational data 
 CREATE TABLE IF NOT EXISTS scrape_runs (
-  -- primary keys and references
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
   -- basic scrape fields
-  started_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  finished_at TEXT,
+  started_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  finished_at TIMESTAMPTZ,
   status TEXT NOT NULL DEFAULT "running" CHECK (
 	status IN ("running", "completed")
   ),
@@ -95,15 +91,21 @@ CREATE TABLE IF NOT EXISTS scrape_runs (
   error_message TEXT,
 
   -- analytics (everything in analytics down is up to your discernment)
-  products_scraped INTEGER,
-  new_products_added INTEGER,
-  retailers_attempted INTEGER, -- maybe
+  products_scraped INTEGER DEFAULT 0,
+  new_products_added INTEGER DEFAULT 0,
+  retailers_attempted INTEGER DEFAULT 0, -- maybe
+
+  CHECK (finished_at IS NOT NULL AND status = "running" || finished_at = NULL AND status = "completed"),
+  CHECK (started_at < finished_at)
 );
 
 CREATE TABLE IF NOT EXISTS retailer_scrapes (
-  -- primary keys and references
-  scrape_run INTEGER NOT NULL,
+  -- natural keys, surrogate id and foreign keys (natural keys just happen to be
+  -- made from the foreign keys)
+  scrape_run_id INTEGER NOT NULL,
   retailer TEXT NOT NULL,
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
   scrape_trapped INTEGER NOT NULL DEFAULT FALSE CHECK(
 	scrape_trapped IN (FALSE, TRUE)
   ),
@@ -121,20 +123,25 @@ CREATE TABLE IF NOT EXISTS retailer_scrapes (
   categories_scraped INTEGER,
   products_scraped INTEGER,
 
-  FOREIGN KEY scrape_run REFERENCES scrape_runs(id),
+  FOREIGN KEY scrape_run_id REFERENCES scrape_runs(id),
   FOREIGN KEY retailer REFERENCES retailers(name),
-  PRIMARY KEY (scrape_run, retailer)
+  UNIQUE (scrape_run_id, retailer),
+  CHECK (finished_at IS NOT NULL AND status = "running" || finished_at = NULL AND status = "completed"),
+  CHECK (started_at < finished_at)
 );
 
 CREATE TABLE IF NOT EXISTS category_scrapes (
-  -- primary keys and references
-  scrape_run INTEGER NOT NULL,
-  retailer TEXT NOT NULL,
+  -- natural keys, surrogate id and foreign keys (natural keys just happen to be made from the
+  -- foreign keys)
+  retailer_scrape_id INTEGER NOT NULL,
+  category_id INTEGER NOT NULL, -- unique because its a foreign key to primary key
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
   name TEXT NOT NULL,
   path TEXT NOT NULL,
   -- basic scrape fields
-  started_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  finished_at TEXT,
+  started_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  finished_at TIMESTAMPTZ,
   status TEXT NOT NULL DEFAULT "running" CHECK (
 	status IN ("running", "completed")
   ),
@@ -146,7 +153,10 @@ CREATE TABLE IF NOT EXISTS category_scrapes (
   total_products_scrapped INTEGER NOT NULL DEFAULT 0,
   total_new_products_found INTEGER NOT NULL DEFAULT 0,
 
-  FOREIGN KEY (scrape_run) REFERENCES retailer_scrapes (scrape_run),
-  FOREIGN KEY (retailer) REFERENCES retailer_scrapes (retailer),
-  PRIMARY KEY (scrape_run, retailer, path)
+  FOREIGN KEY (retailer_scrape_id) REFERENCES retailer_scrapes (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id),
+  CHECK (finished_at IS NOT NULL AND status = "running" || finished_at = NULL AND status = "completed"),
+  CHECK (started_at < finished_at)
 );
+
+COMMIT;
